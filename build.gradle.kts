daabc617ccac85a8ae269c95a08d538020369675
ksolnica <ksolnica@protonmail.com>
2021-07-30 08:59:20 +0200

    Formatting

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	kotlin("jvm") version "1.5.21"
-	java
+    kotlin("jvm") version "1.5.21"
+    java
     application
 }
 
@@ -12,43 +12,43 @@ java.sourceCompatibility = JavaVersion.VERSION_11
 
 application.mainClass.set("com.ks.kafka.KafkaTests")
 tasks.withType<Jar> {
-	manifest {
-		attributes["Main-Class"] = "com.ks.kafka.KafkaTests"
-	}
-	archiveClassifier.set("bundle")
-	from(sourceSets.main.get().output)
-	dependsOn(configurations.runtimeClasspath)
-	from({
-		configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
-	})
-	duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest {
+        attributes["Main-Class"] = "com.ks.kafka.KafkaTests"
+    }
+    archiveClassifier.set("bundle")
+    from(sourceSets.main.get().output)
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.jetbrains.kotlin:kotlin-stdlib")
-	implementation("org.apache.kafka:kafka-clients:2.8.0")
-	implementation("org.apache.kafka:kafka-streams:2.8.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation("org.apache.kafka:kafka-clients:2.8.0")
+    implementation("org.apache.kafka:kafka-streams:2.8.0")
 
-	testImplementation(platform("org.junit:junit-bom:5.7.2"))
-	testImplementation("org.junit.jupiter:junit-jupiter")
-	testImplementation("org.assertj:assertj-core:3.20.2")
-	testImplementation("org.apache.kafka:kafka-streams-test-utils:2.8.0")
+    testImplementation(platform("org.junit:junit-bom:5.7.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.20.2")
+    testImplementation("org.apache.kafka:kafka-streams-test-utils:2.8.0")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
-	testLogging {
-		events("passed", "skipped", "failed")
-	}
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
 }
